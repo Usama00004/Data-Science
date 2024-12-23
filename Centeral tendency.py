ea996728ae4930be878b7c25cbc5e61c5a267806
Usama Tahir <usamatahir00004@gmail.com>
2024-12-23 16:23:12 +0100

    Meaan

--- Centeral tendency.py
+++ Centeral tendency.py
@@ -5,3 +5,13 @@ import numpy as np
 arr = np.array([4,5,2,8,5,9,3,1,9])
 mean = np.sum(arr)/len(arr)
 print(mean)
+
+
+#importing data into dataframe
+dataset = pd.read_csv('Titanic-Dataset.csv')
+
+#Calculating average age from a data set using dataframe
+print(f"Mean Age is {dataset['Age'].mean()} calculculated using Pandas")
+
+#Calculating average age from a data set using Numpy 
+print(f"Mean Age is {np.mean(dataset['Age'])} calculculated using Numpy")
