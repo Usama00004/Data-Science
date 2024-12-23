df678791100cdb7ec72bfc09dc37c5041f5158cb
Usama Tahir <usamatahir00004@gmail.com>
2024-12-23 16:55:20 +0100

    histplot

--- Centeral tendency.py
+++ Centeral tendency.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+
 
 # Calculating mean by formula
 arr = np.array([4,5,2,8,5,9,3,1,9])
@@ -15,3 +18,7 @@ print(f"Mean Age is {dataset['Age'].mean()} calculculated using Pandas")
 
 #Calculating average age from a data set using Numpy 
 print(f"Mean Age is {np.mean(dataset['Age'])} calculculated using Numpy")
+
+#plotting 
+sb.histplot(x ="Age",data=dataset,bins=[i for i  in range (0,81,10)])
+plt.show()
