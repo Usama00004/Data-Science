eaf56ea43f56cc33cc032a2d9d352e92c08ff21d
Usama Tahir <usamatahir00004@gmail.com>
2024-12-23 20:39:09 +0100

    Mean Mode Median

--- Measurement of Centeral tendency.py
+++ Measurement of Centeral tendency.py
@@ -14,29 +14,29 @@ print(mean)
 dataset = pd.read_csv("Titanic-Dataset.csv")
 
 #Calculating mean age from a data set using dataframe
-print(f"Mean Age is {dataset['Age'].mean()} calculculated using Pandas")
+print(f"Mean Age is {dataset['Fare'].mean()} calculculated using Pandas")
 
 #Calculating mean age from a data set using Numpy 
-print(f"Mean Age is {np.mean(dataset['Age'])} calculculated using Numpy")
+print(f"Mean Age is {np.mean(dataset['Fare'])} calculculated using Numpy")
 
 #plotting 
-mean = dataset["Age"].mean()
+mean = dataset["Fare"].mean()
 # sb.histplot(x ="Age",data=dataset,bins=[i for i  in range (0,81,10)])
 # plt.plot([mean for i in range(0,300)],[i for i in range (0,300)], c = "red")
 # plt.show()
 
 
 #################### MEDIAN ####################
-dataset["Age"].fillna(dataset["Age"].mean(), inplace=True)
+dataset["Fare"].fillna(dataset["Fare"].mean(), inplace=True)
 print(dataset.isnull().sum())
 
 #Calculating median age from a data set using dataframe
-print(f"Median Age is {dataset['Age'].median()} calculculated using Pandas")
+print(f"Median Age is {dataset['Fare'].median()} calculculated using Pandas")
 
 #Calculating mean age from a data set using Numpy 
-print(f"Median Age is {np.median(dataset['Age'])} calculculated using Numpy")
+print(f"Median Age is {np.median(dataset['Fare'])} calculculated using Numpy")
 
-median = dataset["Age"].median()
+median = dataset["Fare"].median()
 # sb.histplot(x ="Age",data=dataset,bins=[i for i  in range (0,81,10)])
 # plt.plot([mean for i in range(0,300)],[i for i in range (0,300)], c = "red")
 # plt.plot([median for i in range(0,300)],[i for i in range (0,300)], c = "blue")
@@ -44,8 +44,16 @@ median = dataset["Age"].median()
 
 #################### MODE ####################
 print("======Mode======")
-print(dataset["Age"].mode()[0])
-print(dataset["Age"].value_counts())
+print(dataset["Fare"].mode()[0])
+print(dataset["Fare"].value_counts())
 
+mode = dataset["Fare"].mode()[0]
+
+sb.histplot(x ="Fare",data=dataset,bins=[i for i  in range (0,81,10)])
+plt.plot([mean for i in range(0,300)],[i for i in range (0,300)], c = "red")
+plt.plot([median for i in range(0,300)],[i for i in range (0,300)], c = "blue")
+plt.plot([mode for i in range(0,300)],[i for i in range (0,300)], c = "green")
+
+plt.show()
 
 
