53b0f3d57497f85871d543d82036764706897c15
Usama Tahir <usamatahir00004@gmail.com>
2024-12-27 12:54:41 +0100

    Measure of Variablity

--- Measurement of Variability.py
+++ Measurement of Variability.py
@@ -20,6 +20,7 @@ section_a = np.array([75,65,73,68,72,76])
 section_b = np.array([90,47,43,96,93,51])
 number = np.array([1,2,3,4,5,6])
 
+# Mean is same for both data
 mean = np.mean(section_a)
 
 plt.figure(figsize=(10,3))
@@ -29,4 +30,12 @@ plt.plot([70,70,70,70,70,70],number,c="blue",label="Mean")
 plt.legend()
 plt.show()
 
+# Using formula for mean absoulte devision of section a
+mean_absoulte_devision_a =  np.sum(np.abs(section_a - mean))/len(section_a)
+print(mean_absoulte_devision_a)
+
+
+# Using formula for mean absoulte devision of section b
+mean_absoulte_devision_b =  np.sum(np.abs(section_b - mean))/len(section_b)
+print(mean_absoulte_devision_b)
 
